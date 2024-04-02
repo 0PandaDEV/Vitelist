d8ced5e17be41614aad53ba175a6e90a8d4545c7
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-04-03 00:37:45 +0200

    Initial commit

---
+++ src/main/java/net/pandadev/vitelist/Main.java
@@ -0,0 +1,87 @@
+package net.pandadev.vitelist;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import net.kyori.adventure.text.Component;
+import org.slf4j.Logger;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+
+import java.nio.file.Path;
+import java.util.List;
+
+@Plugin(
+        id = "vitelist",
+        name = "Main",
+        version = "1.0",
+        description = "A simple but useful whitelist plugin for Velocity",
+        url = "https://pandadev.net",
+        authors = {"PandaDEV"}
+)
+public class Main {
+
+    public static String prefix = "§d§lVitelist §8» ";
+    @Inject
+    private Logger logger;
+    @Inject
+    private ProxyServer server;
+    @Inject
+    @DataDirectory
+    private Path dataDirectory;
+    private YamlConfigurationLoader loader;
+    private boolean whitelistEnabled = true;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        loader = YamlConfigurationLoader.builder().path(dataDirectory.resolve("config.yml")).build();
+        try {
+            var root = loader.load();
+            if (!root.node("whitelisted-uuids").virtual()) {
+                whitelistEnabled = root.node("whitelist-enabled").getBoolean(true);
+            } else {
+                root.node("whitelisted-uuids").set(List.of());
+                root.node("whitelist-enabled").set(true);
+                loader.save(root);
+            }
+        } catch (ConfigurateException e) {
+            logger.error("Failed to load/create config file", e);
+        }
+
+        CommandManager commandManager = server.getCommandManager();
+        commandManager.register(commandManager.metaBuilder("vlist").build(), new VlistCommand(this));
+    }
+
+    @Subscribe
+    public void onPlayerJoin(PostLoginEvent event) {
+        if (!whitelistEnabled) return;
+        Player player = event.getPlayer();
+        try {
+            var root = loader.load();
+            List<String> whitelistedUuids = root.node("whitelisted-uuids").getList(String.class);
+            if (!whitelistedUuids.contains(player.getUniqueId().toString())) {
+                player.disconnect(Component.text("You are not whitelisted on this server."));
+            }
+        } catch (ConfigurateException e) {
+            logger.error("Failed to check whitelist", e);
+        }
+    }
+
+    public YamlConfigurationLoader getLoader() {
+        return loader;
+    }
+
+    public void setWhitelistEnabled(boolean whitelistEnabled) {
+        this.whitelistEnabled = whitelistEnabled;
+    }
+
+    public static String getPrefix() {
+        return prefix;
+    }
+}
---
+++ src/main/java/net/pandadev/vitelist/VlistCommand.java
@@ -0,0 +1,246 @@
+package net.pandadev.vitelist;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
+import net.kyori.adventure.text.Component;
+import org.spongepowered.configurate.ConfigurateException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class VlistCommand implements SimpleCommand {
+
+    private final Main plugin;
+
+    public VlistCommand(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void execute(Invocation invocation) {
+        CommandSource source = invocation.source();
+        String[] args = invocation.arguments();
+
+        if (args.length < 1) {
+            source.sendMessage(Component.text(Main.getPrefix() + "§cInvalid usage"));
+            return;
+        }
+        switch (args[0].toLowerCase()) {
+            case "add":
+                if (args.length < 2) {
+                    source.sendMessage(Component.text(Main.getPrefix() + "§cPlease specify a player name to add"));
+                    return;
+                }
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        String uuid = getUUID(args[1]);
+                        if (uuid != null) {
+                            addUuidToWhitelist(uuid, source);
+                        } else {
+                            source.sendMessage(Component.text(Main.getPrefix() + "§cCould not find UUID for player name"));
+                        }
+                    } catch (Exception e) {
+                        source.sendMessage(Component.text(Main.getPrefix() + "§cAn error occurred while processing the command: " + e.getMessage()));
+                    }
+                });
+                break;
+            case "remove":
+                if (args.length < 2) {
+                    source.sendMessage(Component.text(Main.getPrefix() + "§cPlease specify a player name to remove"));
+                    return;
+                }
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        String uuid = getUUID(args[1]);
+                        if (uuid != null) {
+                            removeUuidFromWhitelist(uuid, source);
+                        } else {
+                            source.sendMessage(Component.text(Main.getPrefix() + "§cCould not find UUID for player name"));
+                        }
+                    } catch (Exception e) {
+                        source.sendMessage(Component.text(Main.getPrefix() + "§cAn error occurred while processing the command: " + e.getMessage()));
+                    }
+                });
+                break;
+            case "on":
+                plugin.setWhitelistEnabled(true);
+                source.sendMessage(Component.text(Main.getPrefix() + "§7Vitelist enabled"));
+                break;
+            case "off":
+                plugin.setWhitelistEnabled(false);
+                source.sendMessage(Component.text(Main.getPrefix() + "§7Vitelist disabled"));
+                break;
+            case "list":
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        List<String> uuids = getWhitelistedUuids();
+                        if (!uuids.isEmpty()) {
+                            List<String> playerNames = new ArrayList<>();
+                            for (String uuid : uuids) {
+                                String playerName = getNameFromUUID(uuid);
+                                if (playerName != null) {
+                                    playerNames.add(playerName);
+                                }
+                            }
+                            if (!playerNames.isEmpty()) {
+                                String playerNameList = String.join(", ", playerNames);
+                                source.sendMessage(Component.text("§8----- [ §d§lVitelisted players §8] -----"));
+                                source.sendMessage(Component.text(""));
+                                for (String player : playerNames) {
+                                    source.sendMessage(Component.text("§7" + player));
+                                }
+                                source.sendMessage(Component.text(""));
+                                source.sendMessage(Component.text("§8--------------------------------"));
+                            } else {
+                                source.sendMessage(Component.text(Main.getPrefix() + "§7No players are currently vitelisted"));
+                            }
+                        } else {
+                            source.sendMessage(Component.text(Main.getPrefix() + "§7No UUIDs are currently vitelisted"));
+                        }
+                    } catch (Exception e) {
+                        source.sendMessage(Component.text(Main.getPrefix() + "§cAn error occurred while processing the command: " + e.getMessage()));
+                    }
+                });
+                break;
+            default:
+                source.sendMessage(Component.text(Main.getPrefix() + "§cInvalid command"));
+        }
+    }
+
+    private List<String> getWhitelistedUuids() throws ConfigurateException {
+        var root = plugin.getLoader().load();
+        return root.node("whitelisted-uuids").getList(String.class);
+    }
+
+    private String getNameFromUUID(String uuid) {
+        try {
+            URL url = new URL("https://playerdb.co/api/player/minecraft/" + uuid);
+            return getPlayerNameFromAPI(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getUUID(String name) {
+        try {
+            URL url = new URL("https://playerdb.co/api/player/minecraft/" + name);
+            return getPlayerUUIDFromAPI(url);
+        } catch (Exception e) {
+            System.out.println("Unable to get UUID for: " + name + " due to error: " + e.getMessage());
+        }
+        return null;
+    }
+
+    private static String getPlayerNameFromAPI(URL url) throws Exception {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        int responseCode = conn.getResponseCode();
+        if (responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        } else {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+                JsonObject data = jsonObject.getAsJsonObject("data");
+                JsonObject player = data.getAsJsonObject("player");
+                return player.get("username").getAsString();
+            }
+        }
+    }
+
+    private static String getPlayerUUIDFromAPI(URL url) throws Exception {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        int responseCode = conn.getResponseCode();
+        if (responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        } else {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+                JsonObject data = jsonObject.getAsJsonObject("data");
+                JsonObject player = data.getAsJsonObject("player");
+                return player.get("id").getAsString();
+            }
+        }
+    }
+
+    private void addUuidToWhitelist(String uuid, CommandSource source) {
+        try {
+            var root = plugin.getLoader().load();
+            List<String> uuids = root.node("whitelisted-uuids").getList(String.class);
+            assert uuids != null;
+            if (!uuids.contains(uuid)) {
+                uuids.add(uuid);
+                root.node("whitelisted-uuids").set(uuids);
+                plugin.getLoader().save(root);
+                source.sendMessage(Component.text(Main.getPrefix() + "§7Added §a" + getNameFromUUID(uuid) + " §7to the vitelist"));
+            } else {
+                source.sendMessage(Component.text(Main.getPrefix() + "§a" + getNameFromUUID(uuid) + " §7is already on the vitelist"));
+            }
+        } catch (ConfigurateException e) {
+            source.sendMessage(Component.text(Main.getPrefix() + "§cAn error occurred while processing the command: " + e.getMessage()));
+        }
+    }
+
+    private void removeUuidFromWhitelist(String uuid, CommandSource source) {
+        try {
+            var root = plugin.getLoader().load();
+            List<String> uuids = root.node("whitelisted-uuids").getList(String.class);
+            assert uuids != null;
+            if (uuids.remove(uuid)) {
+                root.node("whitelisted-uuids").set(uuids);
+                plugin.getLoader().save(root);
+                source.sendMessage(Component.text(Main.getPrefix() + "§7Removed §a" + getNameFromUUID(uuid) + " §7from the vitelist"));
+            } else {
+                source.sendMessage(Component.text(Main.getPrefix() + "§a" + getNameFromUUID(uuid) + " §7not found on the vitelist"));
+            }
+        } catch (ConfigurateException e) {
+            source.sendMessage(Component.text(Main.getPrefix() + "§cAn error occurred while processing the command: " + e.getMessage()));
+        }
+    }
+
+    @Override
+    public List<String> suggest(Invocation invocation) {
+        String[] args = invocation.arguments();
+        if (args.length == 0 || (args.length == 1 && args[0].isEmpty())) {
+            return Stream.of("add", "remove", "on", "off", "list").collect(Collectors.toList());
+        } else if (args.length == 1) {
+            return Stream.of("add", "remove", "on", "off", "list")
+                    .filter(s -> s.startsWith(args[0].toLowerCase()))
+                    .collect(Collectors.toList());
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("add")) {
+            return Stream.of("<player>").collect(Collectors.toList());
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("remove")) {
+            return getWhitelistedPlayerNames();
+        }
+        return List.of();
+    }
+
+    private List<String> getWhitelistedPlayerNames() {
+        try {
+            List<String> uuids = getWhitelistedUuids();
+            List<String> playerNames = new ArrayList<>();
+            for (String uuid : uuids) {
+                String playerName = getNameFromUUID(uuid);
+                if (playerName != null) {
+                    playerNames.add(playerName);
+                }
+            }
+            return playerNames;
+        } catch (ConfigurateException e) {
+            e.printStackTrace();
+            return List.of();
+        }
+    }
+}
